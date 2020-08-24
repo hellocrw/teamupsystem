54c926cab72bb851a47cf17fb3087ca8b2d4b06d
hellocrw <2388092655@qq.com>
2020-08-25 07:46:18 +0800

    openFeign

--- Order80/src/main/java/crw/team/order/controller/OrderController.java
+++ Order80/src/main/java/crw/team/order/controller/OrderController.java
@@ -23,8 +23,26 @@ public class OrderController {
         return restTemplate.getForObject(ONLINE_TEAM_URL + "/", String.class);
     }
 
+    /**
+     * 服务调用测试方法
+     * @return
+     */
     @GetMapping("/demo01")
     public String demo(){
         return "Order 80";
+    }
+
+    /**
+     * 服务超时控制测试方法
+     */
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            log.info("服务超时");
+            e.printStackTrace();
+        }
+        return "#paymentFeignTimeout success";
     }
 }
