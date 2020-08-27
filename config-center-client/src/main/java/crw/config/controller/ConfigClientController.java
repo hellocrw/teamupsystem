abb377857de7956ff5b219ad662b9122c9f9d410
hellocrw <2388092655@qq.com>
2020-08-27 22:51:30 +0800

    配置中心

---
+++ config-center-client/src/main/java/crw/config/controller/ConfigClientController.java
@@ -0,0 +1,17 @@
+package crw.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
