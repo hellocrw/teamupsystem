d3768762e234a5e17b10eed03d4bdd64f9d7e024
hellocrw <2388092655@qq.com>
2021-05-16 11:53:04 +0800

    temp

--- cloud-study/src/main/java/crw/cloud/cloudstudy/controller/StudyController.java
+++ cloud-study/src/main/java/crw/cloud/cloudstudy/controller/StudyController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @RestController
 @RequestMapping("/study")
 public class StudyController {
 
     @GetMapping("demo")
     public String demo() {
+        ConcurrentHashMap<String ,Object> concurrentHashMap = new ConcurrentHashMap<String, Object>();
         return Thread.currentThread().getThreadGroup().getName();
     }
 }
